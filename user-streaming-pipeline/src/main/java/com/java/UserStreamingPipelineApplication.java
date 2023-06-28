0e2496f7ad9110956070fe482ee7e2d976460029
pulkitk1234 <pulkitk1403@gmail.com>
2023-06-28 10:57:56 +0530

    initial docker setup

--- user-streaming-pipeline/src/main/java/com/java/UserStreamingPipelineApplication.java
+++ user-streaming-pipeline/src/main/java/com/java/UserStreamingPipelineApplication.java
@@ -6,7 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserStreamingPipelineApplication {
 	
-//	refer -> https://kafka.apache.org/quickstart
+//	refer -> https://kafka.apache.org/quickstart- for kafka refs
+	
+	//docker compose -f docker-compose.yml down // to stop containers
+	//pwd-> /Users/pulkitkaushik/Desktop/brandwatch-repo/streaming/user-streaming-pipeline
+	//	-> run: docker compose -f docker-compose.yml up -d (to start kafka)
+	//pwd-> /Users/pulkitkaushik/Desktop/sts-projects
+	//	-> run:docker compose -f docker-compose.yml up -d(to start postgresql)
+	//start springboot app 
+
 
 	public static void main(String[] args) {
 	
